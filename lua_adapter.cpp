2910a77ee7cade130104e61b76e5106fead161bb
Julian Winter <julian.winter@fh-muenster.de>
2016-09-05 21:54:49 +0200

    Check for adequate row/col-selection in tables. Added debug-output mode

--- lua_adapter.cpp
+++ lua_adapter.cpp
@@ -1,350 +1,396 @@
 #ifndef LUA_ADAPTER_H
-	#include "lua_adapter.hpp"
+    #include "lua_adapter.hpp"
 #endif
 
 LuaAdapter::LuaAdapter()
-	:Lua{nullptr}
-	,loaded{false}		
-	//,error{false}		
+    :Lua{nullptr}
+    ,loaded{false}		
+    ,print{false}
+    ,outputPrefix{"Lua» "}
 {
-	this->init();  			 							
+    this->init();  			 							
 }		
 
+
 void LuaAdapter::init(){
-	if(!this->Lua) {
-		this->Lua = luaL_newstate();
-	   	luaL_openlibs(this->Lua);
-	}				
+    if(this->Lua)
+        return;
+    this->Lua = luaL_newstate();
+    luaL_openlibs(this->Lua);    				
 }
 
 
 bool LuaAdapter::load(const char* name){
-	this->init();
-	
-	if ( luaL_loadfile(this->Lua, name) || lua_pcall(this->Lua, 0, 0, 0) ){
-		 std::cout << "Error in Lua-file '" << name << "': ";
-		 std::cout << lua_tostring(this->Lua, -1);
-		 std::cout << "\n";
-		  this->loaded=false;
-		  return false;
-	 }	
-	this->loaded = true;
-	return true;
+    this->init();
+
+    if ( luaL_loadfile(this->Lua, name) || lua_pcall(this->Lua, 0, 0, 0) ){
+        std::cout << "Error in Lua-file '" << name << "': ";
+        std::cout << lua_tostring(this->Lua, -1);
+        std::cout << "\n";
+        this->loaded=false;
+        return false;
+    }	
+    this->loaded = true;
+    return true;
 }
 
 
 bool LuaAdapter::getField(const char* name){
-	if((!this->Lua)
-	|| (!this->loaded)
-	|| (!name)
-	|| (!lua_istable(this->Lua, -1))
-	)		 		
-	 	return false;		 	
-	 
-	lua_getfield(this->Lua, -1, name);
-	return true;		
+    if((!this->Lua)
+    || (!this->loaded)
+    || (!name)
+    || (!lua_istable(this->Lua, -1))
+    )		 		
+        return false;		 	
+
+    lua_getfield(this->Lua, -1, name);
+    return true;		
 }
 
 
 bool LuaAdapter::getI(unsigned short int i){
-	if((!this->Lua)
-	|| (!this->loaded)            
-	|| (!lua_istable(this->Lua, -1))
-	)		 		
-	 	return false;		 	
-	 
-	lua_rawgeti(this->Lua, -1, i);
-	return true;		
+    if((!this->Lua)
+    || (!this->loaded)            
+    || (!lua_istable(this->Lua, -1))
+    || (i<1)
+    )		 		
+        return false;		 	
+
+    lua_rawgeti(this->Lua, -1, i);
+    return true;		
 }
 
 bool LuaAdapter::getGlobal(const char* name){
-	if((!this->Lua)
-	|| (!this->loaded)
-	|| (!name)
-	)		 		
-		return false;		 	
-	 	
-	 lua_getglobal(this->Lua, name); 
-	return true;		
+    if((!this->Lua)
+    || (!this->loaded)
+    || (!name)
+    )		 		
+        return false;		 	
+
+    lua_getglobal(this->Lua, name); 
+    return true;		
 }
 
 
 bool LuaAdapter::get(const char *name, int &result){
-	if(this->getGlobal(name)==false)
-		return false;
-	 
-	if ( lua_isnumber(this->Lua, -1)==false) {
-		lua_pop(this->Lua, 1);			
-		return false;   						
-	}
-	result = lua_tointeger(this->Lua, -1);            
-	lua_pop(this->Lua, 1);
-	return true;	
+    if(this->getGlobal(name)==false)
+        return false;
+
+    if ( lua_isnumber(this->Lua, -1)==false) {
+        lua_pop(this->Lua, 1);			
+        return false;   						
+    }
+    result = lua_tointeger(this->Lua, -1);
+    if(this->print)
+        std::cout << this->outputPrefix << "get int '" << name << "' = '" << result << "' \n";
+    lua_pop(this->Lua, 1);
+    return true;	
 }
 
 
 bool LuaAdapter::get(const char *name, std::string &result) {
-	 if(this->getGlobal(name)==false)
-		return false;
-	 
-	if ( lua_isstring(this->Lua, -1)==false) {
-		lua_pop(this->Lua, 1);			
-		return false;   						
-	}
-	result = lua_tostring(this->Lua, -1);            
-	lua_pop(this->Lua, 1);
-	return true;
+    if(this->getGlobal(name)==false)
+        return false;
+
+    if ( lua_isstring(this->Lua, -1)==false) {
+        lua_pop(this->Lua, 1);			
+        return false;   						
+    }
+    result = lua_tostring(this->Lua, -1);
+    if(this->print)
+        std::cout << this->outputPrefix << "get string '" << name << "' = '" << result << "' \n";          
+    lua_pop(this->Lua, 1);
+    return true;
 }
 
 
 bool LuaAdapter::get(const char *name, double &result) {
-	if(this->getGlobal(name)==false)
-		return false;
-	 
-	if ( lua_isnumber(this->Lua, -1)==false) {
-		lua_pop(this->Lua, 1);			
-		return false;   						
-	}
-	result = lua_tonumber(this->Lua, -1);            
-	lua_pop(this->Lua, 1);
-	return true;		
+    if(this->getGlobal(name)==false)
+        return false;
+
+    if ( lua_isnumber(this->Lua, -1)==false) {
+        lua_pop(this->Lua, 1);			
+        return false;   						
+    }
+    result = lua_tonumber(this->Lua, -1);
+    if(this->print)
+        std::cout << this->outputPrefix << "get double '" << name << "' = '" << result << "' \n";          
+        lua_pop(this->Lua, 1);
+    return true;		
 } 
 
 
 bool LuaAdapter::get(const char *name, float &result) {
-	double temp = 0.0;
-	if(this->get(name, temp)==false)
-		return false;
-	result = (float)temp;           
-	return true;		
+    double temp = 0.0;
+    if(this->get(name, temp)==false)
+        return false;
+    result = (float)temp;             
+    return true;		
 } 
 
 
 bool LuaAdapter::openTable(const char *name) {		 		 	
-	return this->getGlobal(name) && lua_istable(this->Lua, -1);
+    if(this->getGlobal(name) && lua_istable(this->Lua, -1)){
+        if(this->print)
+            std::cout << this->outputPrefix << "open table '" << name << "' \n";
+        return true;
+    }
+    return false;
 }
 
 
 bool LuaAdapter::openNestedTable(const char *name){			
-	if((!this->Lua) || (!this->loaded) ){		 		
- 		return false;
- 	}
+    if((!this->Lua) || (!this->loaded) ){		 		
+        return false;
+    }
 
-	if(lua_istable(this->Lua, -1) == false) {				
-		return false;
-	}		
-	 	
-	lua_getfield(this->Lua, -1, name);
-	
-	if (lua_isnil(this->Lua, -1)) {
-		lua_pop(this->Lua, 1);
-		return false;
-	}
-	return lua_istable(this->Lua, -1);
+    if(lua_istable(this->Lua, -1) == false) {				
+        return false;
+    }		
+
+    lua_getfield(this->Lua, -1, name);
+
+    if (lua_isnil(this->Lua, -1)) {
+        lua_pop(this->Lua, 1);
+        return false;
+    }
+
+    if(lua_istable(this->Lua, -1)){
+    if(this->print)
+        std::cout << "\t" << this->outputPrefix << "open nested table '" << name << "' \n";
+        return true;
+    }
+    return false;
 }
 
 
 bool LuaAdapter::flush(){
-	if((!this->Lua) || (!this->loaded) )
-	 	return false;
-		 	
-	lua_settop(this->Lua, 0);
-	return true;
+    if((!this->Lua) || (!this->loaded) )
+        return false;
+
+    lua_settop(this->Lua, 0);
+    return true;
 }
-	
+
+
+void LuaAdapter::setLogMode(bool mode){
+    this->print = mode;	
+} 
+
 
 bool LuaAdapter::getField(const char *name, std::string &result) {
-	if(this->getField(name)==false)            	 
-		return false;		 		
-	
-	if (lua_isstring(this->Lua, -1) != 1) {
-		lua_pop(this->Lua, 1);	
-		return false;
-	}
-	result = lua_tostring(this->Lua, -1);
-	lua_pop(this->Lua, 1);			
-	return true;
+    if(this->getField(name)==false)            	 
+        return false;		 		
+
+    if (lua_isstring(this->Lua, -1) != 1) {
+        lua_pop(this->Lua, 1);	
+        return false;
+    }
+    result = lua_tostring(this->Lua, -1);
+    if(this->print)
+        std::cout << "\t"  << this->outputPrefix << "get string-field '" << name << "' = '" << result << "' \n";
+    lua_pop(this->Lua, 1);			
+    return true;
 }
 
 
 bool LuaAdapter::getField(const char *name, int &result) {
-	if(this->getField(name)==false)            	 
-	return false;            
-	if (lua_isnumber(this->Lua, -1) != 1) {
-		lua_pop(this->Lua, 1);	
-		return false;
-	}            
-	result = lua_tointeger(this->Lua, -1);
-	lua_pop(this->Lua, 1);			
-	return true;
+    if(this->getField(name)==false)            	 
+        return false;            
+    if (lua_isnumber(this->Lua, -1) != 1) {
+        lua_pop(this->Lua, 1);	
+        return false;
+    }            
+    result = lua_tointeger(this->Lua, -1);
+    if(this->print)
+        std::cout << "\t"  << this->outputPrefix << "get int-field '" << name << "' = '" << result << "' \n";
+    lua_pop(this->Lua, 1);			
+    return true;
 }
 
 
 bool LuaAdapter::getField(unsigned short int i, int &result){	
-	if(this->getI(i)==false)            	 
-		return false;
-	
-	if (lua_isnumber(this->Lua, -1) != 1) {
-		lua_pop(this->Lua, 1);	
-		return false;
-	}
-	result = lua_tointeger(this->Lua, -1);
-	lua_pop(this->Lua, 1);            
-	return true;
+    if(this->getI(i)==false)            	 
+        return false;
+
+    if (lua_isnumber(this->Lua, -1) != 1) {
+        lua_pop(this->Lua, 1);	
+        return false;
+    }
+    result = lua_tointeger(this->Lua, -1);
+    if(this->print)
+        std::cout << "\t"  << this->outputPrefix << "get int-field " << i << " = '" << result << "' \n";
+    lua_pop(this->Lua, 1);            
+    return true;
 }
 
 
 bool LuaAdapter::getField(unsigned short int i, double &result){	
-	if(this->getI(i)==false)            	 
-		return false;
-	
-	if (lua_isnumber(this->Lua, -1) != 1) {
-		lua_pop(this->Lua, 1);	
-		return false;
-	}
-	result = lua_tonumber(this->Lua, -1);
-	lua_pop(this->Lua, 1);            
-	return true;
+    if(this->getI(i)==false)            	 
+        return false;
+
+    if (lua_isnumber(this->Lua, -1) != 1) {
+        lua_pop(this->Lua, 1);	
+        return false;
+    }
+    result = lua_tonumber(this->Lua, -1);
+    if(this->print)
+        std::cout << "\t"  << this->outputPrefix << "get double-field " << i << " = '" << result << "' \n";
+    lua_pop(this->Lua, 1);            
+    return true;
 }
 
 
 bool LuaAdapter::getField(unsigned short int i, float &result){	
-	if(this->getI(i)==false)            	 
-		return false;
-	
-	if (lua_isnumber(this->Lua, -1) != 1) {
-		lua_pop(this->Lua, 1);	
-		return false;
-	}
-	result = (float)lua_tonumber(this->Lua, -1);
-	lua_pop(this->Lua, 1);            
-	return true;
+    if(this->getI(i)==false)            	 
+        return false;
+
+    if (lua_isnumber(this->Lua, -1) != 1) {
+        lua_pop(this->Lua, 1);	
+        return false;
+    }
+    result = (float)lua_tonumber(this->Lua, -1);
+    if(this->print)
+        std::cout << "\t"  << this->outputPrefix << "get float-field " << i << " = '" << result << "' \n";
+    lua_pop(this->Lua, 1);            
+    return true;
 }
 
 
 bool LuaAdapter::set(const char *name, const char *value){
-	if(!this->Lua){		 		
-	 	return false;
-	 }
-	lua_pushstring(this->Lua, value);
-	lua_setglobal(this->Lua, name);			
-	return true;
+    if(!this->Lua)		 		
+        return false;    
+    lua_pushstring(this->Lua, value);
+    lua_setglobal(this->Lua, name);
+    if(this->print)
+        std::cout << this->outputPrefix << "set string '" << name << "' = '" << value << "' \n";		
+    return true;
 }	
 
 
 bool LuaAdapter::set(const char *name, int value){
-	if(!this->Lua){		 		
-	 	return false;
-	 }
-	lua_pushnumber(this->Lua, value);
-	lua_setglobal(this->Lua, name);			
-	return true;
+    if(!this->Lua){		 		
+        return false;
+    }
+    lua_pushnumber(this->Lua, value);
+    lua_setglobal(this->Lua, name);
+    if(this->print)
+        std::cout << this->outputPrefix << "set int '" << name << "' = '" << value << "' \n";				
+    return true;
 }
 
 
 bool LuaAdapter::set(const char *name, const double value){
-	if(!this->Lua){		 		
-	 	return false;
-	 }
-	lua_pushnumber(this->Lua, value);
-	lua_setglobal(this->Lua, name);			
-	return true;
+    if(!this->Lua)	 		
+        return false;    
+    lua_pushnumber(this->Lua, value);
+    lua_setglobal(this->Lua, name);
+    if(this->print)
+        std::cout << this->outputPrefix << "set double '" << name << "' = '" << value << "' \n";				
+    return true;
 }	
 
 
 bool LuaAdapter::set(const char *name, const float value){
-	return this->set(name, (double)value);
+    return this->set(name, (double)value);
 }
 
 
 bool LuaAdapter::getNestedField(unsigned short int j, unsigned short int i, double &result){	
-	if(this->getI(j)==false) {
-	lua_pop(this->Lua, 1);
-		return false;
-	}            
-	if(this->getI(i)==false) {
-	lua_pop(this->Lua, 2);
-	return false;
-}
-
-    result = lua_tonumber(this->Lua, -1);            
+    if(this->getI(j)==false) {
+        lua_pop(this->Lua, 1);
+        return false;
+    }            
+    if(this->getI(i)==false) {
+        lua_pop(this->Lua, 2);
+        return false;
+    }
+    if(lua_isnumber(this->Lua, -1))
+        result = lua_tonumber(this->Lua, -1);
+    if(this->print)
+        std::cout << "\t"  << "\t"  << this->outputPrefix << "get nested double-field (" << j << "|"<< i << ")' = '" << result << "' \n";
     lua_pop(this->Lua, 2);	
     return true;
 }
 
 
 bool LuaAdapter::getNestedField(unsigned short int j, unsigned short int i, float &result){	
-	if(this->getI(j)==false) {
-		lua_pop(this->Lua, 1);
-		return false;
-	}            
-	if(this->getI(i)==false) {
-		lua_pop(this->Lua, 2);
-	return false;
-	}
-	
-	result = (float)lua_tonumber(this->Lua, -1);            
-	lua_pop(this->Lua, 2);	
-	return true;
+    if(this->getI(j)==false) {
+        lua_pop(this->Lua, 1);
+        return false;
+    }            
+    if(this->getI(i)==false) {
+        lua_pop(this->Lua, 2);
+        return false;
+    }
+    if(lua_isnumber(this->Lua, -1))
+        result = (float)lua_tonumber(this->Lua, -1);     
+    if(this->print)
+        std::cout << "\t"  << "\t"  << this->outputPrefix << "get nested float-field (" << j << "|"<< i << ")' = '" << result << "' \n";          
+    lua_pop(this->Lua, 2);	
+    return true;
 }
 
 
 bool LuaAdapter::getNestedField(unsigned short int j, unsigned short int i, int &result){	
-	if(this->getI(j)==false) {
-		lua_pop(this->Lua, 1);
-		return false;
-	}            
-	if(this->getI(i)==false) {
-		lua_pop(this->Lua, 2);
-		return false;
-	}
-	result = lua_tointeger(this->Lua, -1);            
-	lua_pop(this->Lua, 2);	
-	return true;
+    if(this->getI(j)==false) {
+        lua_pop(this->Lua, 1);
+        return false;
+    }            
+    if(this->getI(i)==false) {
+        lua_pop(this->Lua, 2);
+        return false;
+    }
+    if(lua_isnumber(this->Lua, -1))
+        result = lua_tointeger(this->Lua, -1);   
+    if(this->print)
+        std::cout << "\t"  << "\t"  << this->outputPrefix << "get nested int-field (" << j << "|"<< i << ")' = '" << result << "' \n";             
+    lua_pop(this->Lua, 2);	
+    return true;
 }
 
 
 bool LuaAdapter::callFunction(const char *pFunctionName, const unsigned short int argc, int args[], int &result){
-	if(!this->loaded) {				
-		return false;
-	}		
-	lua_getglobal(this->Lua, pFunctionName);
-	
-	for(unsigned char i=0; i<argc; i++)
-	   if(args + i)
-	   	lua_pushnumber(this->Lua, args[i]);
-	
-	if (
-		(lua_pcall(this->Lua, argc, 1, 0) != LUA_OK)
-	||  	(lua_isnumber(this->Lua, -1) == false)
-	){
-		lua_pop(this->Lua, 1);				
-		return false;
-	}	            
-	result = lua_tointeger(this->Lua, -1);
-	lua_pop(this->Lua, 1);
-	return false;
+    if(!this->loaded) {				
+        return false;
+    }		
+    lua_getglobal(this->Lua, pFunctionName);
+
+    for(unsigned char i=0; i<argc; i++)
+    if(args + i)
+        lua_pushnumber(this->Lua, args[i]);
+
+    if (
+        (lua_pcall(this->Lua, argc, 1, 0) != LUA_OK)
+    ||	(lua_isnumber(this->Lua, -1) == false)
+    ){
+        lua_pop(this->Lua, 1);				
+        return false;
+    }	            
+    result = lua_tointeger(this->Lua, -1);
+    lua_pop(this->Lua, 1);
+    return false;
 }
 
 
 void LuaAdapter::pushFunction(Lua_callback_function pFunction, const char *pFunctionName) {			
-	lua_pushcfunction(this->Lua, pFunction); 
-	lua_setglobal(this->Lua, pFunctionName);  
+    lua_pushcfunction(this->Lua, pFunction); 
+    lua_setglobal(this->Lua, pFunctionName);  
 }
 
 
 void LuaAdapter::close(){
-	if((!this->Lua) || (!this->loaded))
-		return;
-	this->flush();
-	lua_close(this->Lua); 
-	
-	this->loaded = false;
-	this->Lua = nullptr;
+    if((!this->Lua) || (!this->loaded))
+        return;
+    this->flush();
+    lua_close(this->Lua); 
+
+    this->loaded = false;
+    this->Lua = nullptr;
 }
 
 
 LuaAdapter::~LuaAdapter(){
-	this->close();
+    this->close();
 }	
 
--- lua_adapter.hpp
+++ lua_adapter.hpp
@@ -10,172 +10,180 @@
 
 class LuaAdapter {
 	
-	private:
-		lua_State *Lua;
-		bool loaded;
-	        bool getField(const char* name);
-	        bool getI(unsigned short int i);
-	        bool getGlobal(const char* name);        
-		
-	public:	
-	
-		 LuaAdapter();
-	        ~LuaAdapter();	
-	
-	       /**
-	         * Initialize Lua and all its libs
-	         * Does NOT load any lua-sourcefile
-	         * Set your default (local) vars after calling this function!         
-	         */
-		void init(); 
-	
-	        /** 
-	         * Loads a *.lua-sourcefile         
-	         * Set your default (local) lua-vars before you call this function!
-	         * @param name lua-filename
-	         * @return true on success, false on error
-	         */
-		bool load(const char* name);
-	
-		/**
-	         * @param name Name of the variable inside loaded .lua-file
-	         * @param result value is saved in this variable 
-	         * @return true on success, false on error
-	         */
-		bool get(const char* name, int &result);       	
-		bool get(const char* name, std::string &result);        
-	        bool get(const char* name, double &result);
-	        bool get(const char* name, float &result);
-	
-	        /**
-	         * Opens a lua-table
-	         * @param name Name of the table inside loaded .lua-file         
-	         * @return true on success, false on error
-	         */
-		bool openTable(const char* name);
-	
-	         /**
-	         * Wrapper-function for bool openTable(const char* name);
-	         * @param name Name of the table inside loaded .lua-file         
-	         * @return true on success, false on error
-	         */
-		bool openTable(std::string name){
-			return this->openTable(name.c_str());
-		}
-	
-	        /**
-	         * Opens a table(-field) inside a table
-	         * @param name Name of the table inside the opened table       
-	         * @return true on success, false on error
-	         */
-	        bool openNestedTable(const char *name);        
-	
-	        /**
-	         * Gets a field from an opened table
-	         * @param name Name of the field
-	         * @param result value of the field
-	         * @return true on success, false on error
-	         */
-	        bool getField(const char *name, std::string &result);        
-	        bool getField(const char *name, int &result);
-	
-	        /**
-	         * Gets a field from an opened table
-	         * @param i i-th field (starting at 1)
-	         * @param result value of the field
-	         * @return true on success, false on error
-	         */
-	        bool getField(unsigned short int i, int &result);
-	        bool getField(unsigned short int i, double &result);
-	        bool getField(unsigned short int i, float &result);
-	
-	        /**
-	         * Gets a ("2D"-)field value from an opened table.
-	         * NOTE: DO NOT CALL openNestedTable(name) for this!
-	         * Example: identity ={
-	         *               {1, 0, 0},
-	         *               {0, 1, 0},
-	         *               {0, 0, 1},                       
-	         *           }
-	         * @param j row
-	         * @param i col
-	         * @param result value of the field
-	         * @return true on success, false on error
-	         */
-		bool getNestedField(unsigned short int j, unsigned short int i, double &result);
-	        bool getNestedField(unsigned short int j, unsigned short int i, float &result);
-	        bool getNestedField(unsigned short int j, unsigned short int i, int &result);
-	        
-	        /**
-	         * Sets the value of global lua-var.
-	         * @param name of the variable
-	         * @param value the var's value
-	         * @return true on success, false on error       
-	         */
-	        bool set(const char *name, const char *value);        
-		bool set(const char *name, int value);
-	        bool set(const char *name, const double value);
-	        bool set(const char *name, const float value);
-	        
-	        /**
-	         * Closes a table
-	         * Call this function after every opening (and use) of a table!
-	         * (Prevents "stack-smashing".)         
-	         */
-	        void closeTable(){
-	            return this->pop(1);
-	        }
-	
-	        /**
-	         * Calls a lua-function
-	         * @param name of the function
-	         * @param argc number of arguments passed to the function
-	         * @param args arguments
-	         * @param result returned value of the function
-	         * @return true on success, false on error       
-	         */
-	        bool callFunction(const char* name, const unsigned short int argc, int args[], int &result);
-	
-	        /**
-	         *   
-	         */
-	        void pushFunction(Lua_callback_function, const char*) ;
-	
-	         /**
-	         * Resets Lua's internal stack
-	         * @return true on success, false on error      
-	         */
-		bool flush();   
-	
-	         /**
-	         * Pops i entries from Lua's internal stack
-	         * @param i number of entries     
-	         */
-	   	void pop(signed short int i=1) {
-			lua_pop(this->Lua, i);		
-		}
-	
-	        /**
-	         * Gets the stack position
-	         * @return the stack position
-	         */
-		int getTop() const {
-			return lua_gettop(this->Lua);
-		}
-	
-	        /**
-	         * Gets the value-type of the current stack position
-	         * @return the type
-	         */
-	        int getType() const {
-	            return lua_type(this->Lua, 0);
-	        }
-	
-	        /** 
-	        * "Closes" the loaded *.lua-sourcefile
-	        * This function is called by the destructor of this class!
-	        * */		
-		void close();		
-};
+    private:
+        lua_State *Lua;
+        bool loaded;
+        bool print;
+        const std::string outputPrefix;
+        bool getField(const char* name);
+        bool getI(unsigned short int i);
+        bool getGlobal(const char* name);               
 
-#endif
+    public:	
+
+    LuaAdapter();
+    ~LuaAdapter();	
+
+    /**
+    * Initialize Lua and all its libs
+    * Does NOT load any lua-sourcefile
+    * Set your default (local) vars after calling this function!         
+    */
+    void init(); 
+
+    /** 
+    * Loads a *.lua-sourcefile         
+    * Set your default (local) lua-vars before you call this function!
+    * @param name lua-filename
+    * @return true on success, false on error
+    */
+    bool load(const char* name);
+
+    /**
+    * @param name Name of the variable inside loaded .lua-file
+    * @param result value is saved in this variable 
+    * @return true on success, false on error
+    */
+    bool get(const char* name, int &result);       	
+    bool get(const char* name, std::string &result);        
+    bool get(const char* name, double &result);
+    bool get(const char* name, float &result);
+
+    /**
+    * Opens a lua-table
+    * @param name Name of the table inside loaded .lua-file         
+    * @return true on success, false on error
+    */
+    bool openTable(const char* name);
+
+    /**
+    * Wrapper-function for bool openTable(const char* name);
+    * @param name Name of the table inside loaded .lua-file         
+    * @return true on success, false on error
+    */
+    bool openTable(std::string name){
+    return this->openTable(name.c_str());
+    }
+
+    /**
+    * Opens a table(-field) inside a table
+    * @param name Name of the table inside the opened table       
+    * @return true on success, false on error
+    */
+    bool openNestedTable(const char *name);        
+
+    /**
+    * Gets a field from an opened table
+    * @param name Name of the field
+    * @param result value of the field
+    * @return true on success, false on error
+    */
+    bool getField(const char *name, std::string &result);        
+    bool getField(const char *name, int &result);
+
+    /**
+    * Gets a field from an opened table
+    * @param i i-th field (starting at 1)
+    * @param result value of the field
+    * @return true on success, false on error
+    */
+    bool getField(unsigned short int i, int &result);
+    bool getField(unsigned short int i, double &result);
+    bool getField(unsigned short int i, float &result);
+
+    /**
+    * Gets a ("2D"-)field value from an opened table.
+    * NOTE: DO NOT CALL openNestedTable(name) for this!
+    * Example: identity ={
+    *               {1, 0, 0},
+    *               {0, 1, 0},
+    *               {0, 0, 1},                       
+    *           }
+    * @param j row
+    * @param i col
+    * @param result value of the field
+    * @return true on success, false on error
+    */
+    bool getNestedField(unsigned short int j, unsigned short int i, double &result);
+    bool getNestedField(unsigned short int j, unsigned short int i, float &result);
+    bool getNestedField(unsigned short int j, unsigned short int i, int &result);
+
+    /**
+    * Sets the value of global lua-var.
+    * @param name of the variable
+    * @param value the var's value
+    * @return true on success, false on error       
+    */
+    bool set(const char *name, const char *value);        
+    bool set(const char *name, int value);
+    bool set(const char *name, const double value);
+    bool set(const char *name, const float value);
+
+    /**
+    * Closes a table
+    * Call this function after every opening (and use) of a table!
+    * (Prevents "stack-smashing".)         
+    */
+    void closeTable(){
+    return this->pop(1);
+    }
+
+    /**
+    * Calls a lua-function
+    * @param name of the function
+    * @param argc number of arguments passed to the function
+    * @param args arguments
+    * @param result returned value of the function
+    * @return true on success, false on error       
+    */
+    bool callFunction(const char* name, const unsigned short int argc, int args[], int &result);
+
+    /**
+    *   
+    */
+    void pushFunction(Lua_callback_function, const char*) ;
+
+    /**
+    * Resets Lua's internal stack
+    * @return true on success, false on error      
+    */
+    bool flush();
+
+    /**
+    * Set debug output
+    * @param mode if mode==true, lua-adapter will print out everything	         
+    */
+    void setLogMode(bool mode);  
+
+    /**
+    * Pops i entries from Lua's internal stack
+    * @param i number of entries     
+    */
+    void pop(signed short int i=1) {
+    lua_pop(this->Lua, i);		
+    }
+
+    /**
+    * Gets the stack position
+    * @return the stack position
+    */
+    int getTop() const {
+    return lua_gettop(this->Lua);
+    }
+
+    /**
+    * Gets the value-type of the current stack position
+    * @return the type
+    */
+    int getType() const {
+    return lua_type(this->Lua, 0);
+    }
+
+    /** 
+    * "Closes" the loaded *.lua-sourcefile
+    * This function is called by the destructor of this class!
+    * */		
+    void close();		
+    };
+
+    #endif
