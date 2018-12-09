48af3de75590f52997c467e8ed8239823955cac3
Julian Winter <julian.winter@fh-muenster.de>
2018-12-09 15:10:25 +0100

    Removed *.hpp files in example-compilation instructions.

--- examples/testrun_everything.sh
+++ examples/testrun_everything.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-g++-8 ../LuaAdapter.hpp test.cpp -std=c++17 -llua -ldl -Wall
+g++-8 test.cpp -std=c++17 -llua -ldl -Wall
 ./a.out
 
-g++-8 ../LuaTable.hpp tables.cpp -std=c++17 -llua -ldl -Wall
+g++-8 tables.cpp -std=c++17 -llua -ldl -Wall
 ./a.out
 
-g++-8 ../LuaFunction.hpp functions.cpp -std=c++17 -llua -ldl -Wall -fconcepts
+g++-8 functions.cpp -std=c++17 -llua -ldl -Wall -fconcepts
 ./a.out
