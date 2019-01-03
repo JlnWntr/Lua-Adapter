c36a5c79e2b477eae79de8911bc437e332a18998
Julian Winter <julian.winter@fh-muenster.de>
2019-01-03 17:50:31 +0100

    Added address-sanitizer flag for test-compilation.

--- examples/testrun_everything.sh
+++ examples/testrun_everything.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-g++-8 test.cpp -std=c++17 -llua -ldl -Wall
+g++-8 test.cpp -std=c++17 -llua -ldl -Wall -g -fsanitize=address
 ./a.out
 
-g++-8 tables.cpp -std=c++17 -llua -ldl -Wall
+g++-8 tables.cpp -std=c++17 -llua -ldl -Wall -g -fsanitize=address
 ./a.out
 
-g++-8 functions.cpp -std=c++17 -llua -ldl -Wall -fconcepts
+g++-8 functions.cpp -std=c++17 -llua -ldl -Wall -fconcepts -g -fsanitize=address
 ./a.out
